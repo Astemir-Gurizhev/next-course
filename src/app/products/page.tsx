a852a7081e047f9abdab5aff7a002220998a7874
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-03-17 14:23:21 +0300

    Image component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image'
+
 export default function Page() {
-	return <h1>Products</h1>
+	return (
+		<>
+			<h1>Products</h1>
+			<Image src='/next.svg' alt='' width={100} height={100} />
+		</>
+	)
 }
